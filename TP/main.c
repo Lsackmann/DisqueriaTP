6fb1a841a1ded0b74ae12beebbe39a51e474a40e
SLampropulos <33138794+SLampropulos@users.noreply.github.com>
2017-10-31 20:57:56 -0300

    Update main.c

--- TP/main.c
+++ TP/main.c
@@ -12,9 +12,6 @@
 .Titulo: maximo de 30 caracteres
 .Track: cada titulo puede tener 25 tracks como maximo
 .Precio: real
-
-
-
 */
 
 #define mf 500
@@ -26,7 +23,6 @@ typedef struct
 	int tracks;
 	int precio;
 }	strEstructura; 
-
 
 //------------------------------ CARGA ----------------------------------------------//
 bool chequeo_salida(strEstructura vector[], int i)  //Funcion usada para chequear el ingreso del usuario
@@ -43,7 +39,7 @@ bool chequeo_salida(strEstructura vector[], int i)  //Funcion usada para chequea
 
 void ingreso_cod(strEstructura vector[], int i)
 {	
-	printf ("\n Ingrese un codigo entre 1000 y 8000 para el disco numero %d: ", i+1);
+	printf ("\n Ingrese un codigo entre 1000 y 8000 para el disco numero %d:\nIngrese 9999 para finalizar", i+1);
 	scanf("%i", &vector[i].cod);
 	
 		while (vector[i].cod<1000 || vector[i].cod>8000 && vector[i].cod!=9999) 
@@ -80,7 +76,7 @@ void ingreso_titulo(strEstructura vector[], int i)
 
 void ingreso_tracks(strEstructura vector[], int i)
 {
-	printf("\n Ingrese el numero de tracks que tendra el disco numero %d, recuerde que un disco puede tener como maximo 25 tracks: ", i);
+	printf("\n Ingrese el numero de tracks que tendra el disco numero %d, recuerde que un disco puede tener como maximo 25 tracks: ", i+1);
 	scanf("%d", &vector[i].tracks);
 	
 	while ((vector[i].tracks)>25)
@@ -128,7 +124,7 @@ esto lo hacemos para aprovechar y recorrer el vector una sola vez */
 	
 	largo_vector=i-1;
 	
-	printf("\n La carga a finalizado!");
+	printf("\n La carga ha finalizado!\n");
 	
 	return largo_vector;
 
@@ -207,13 +203,11 @@ void mostrar_datos(strEstructura vector[], int pos, char *mensaje)
 	//posicion
 	printf("\n El %s se encontraba en la posicion %d del vector. \n", mensaje, pos);
 	//titulo
-	printf("\n El titulo del disco es: %s. \n", vector[pos].titulo);
+	printf("\n El titulo del disco es: %s. ", vector[pos].titulo);
 	//tracks	
 	printf ("\n El numero de tracks del disco de %s es : %d . \n", mensaje, vector[pos].tracks);
 	//precio
 	printf("\n El precio del disco %s es :%d.\n", mensaje, vector[pos].precio);
-	
-	printf("\n Estos fueron todos los datos para el campo solicitado\n");
 	return;
 }
 //------------------------------MOSTRAR  ----------------------------------------------//}
@@ -224,22 +218,21 @@ void ordenar_vector(strEstructura vector[], int largo_vector)
 {
 	
 	int i=0;
-	int j;
-	strEstructura aux ;
+	int j,aux ;
 	bool cambio=true;
 	
 	while (i<=largo_vector-1 && cambio)
 	{	
 		cambio=false;
 		
-		for (j=0; j<largo_vector-i; j++)
+		for (j=0; j<=largo_vector-i; j++)
 			{
 			if (vector[j].cod > vector[j+1].cod)
 				{	
 					cambio=true;
-					aux= vector[j] ;
-					vector[j]=vector[j+1];
-					vector[j+1]= aux;
+					aux= vector[j].cod ;
+					vector[j].cod=vector[j+1].cod;
+					vector[j+1].cod= aux;
 				}		
 			
 			}	
@@ -265,86 +258,136 @@ void mostrar_datos_vector_ordenado(strEstructura vector[], int largo_vector)
 //------------------------------ BUSQUEDA BINARIA ----------------------------------------------//
 int ingreso_busqueda_binaria(strEstructura vector[], int largo_vector)
 {
-	int inf=0;
-	int sup=largo_vector;
+	int inf = 0;
+	int sup = largo_vector;
 	int centro;
 	int dato;
+	bool bandera = false;
+	int num;
 	
-	printf("Por favor, ingrese el numero a buscar: \n");
-	scanf("%d", &dato);
-	
-	
-	while (inf <= sup)
-	{
-		centro= (inf+sup)/2;
+	printf("si desea informacion de un CD en especial introdusca 1, si no introduaca otro numero: ");
+	scanf("%i", &num);
 		
-		if ((vector[centro].cod)==dato)
-		{	
-			printf("El valor fue encontrado, y su información es: ");
-			mostrar_datos(vector, centro , "buscado");
-			return centro;
-		}
-		else
+	if(num == 1){
+		
+		printf("Por favor, ingrese el codigo del CD que buscar: \n");
+		scanf("%d", &dato);
+	
+		while (inf <= sup)
 		{
-			if (dato<vector[centro].cod)
-				sup=centro-1;
-			else 
-				inf=centro+1;
-		}
+			centro = (inf+sup)/2;
 		
+			if ((vector[centro].cod) == dato)
+			{	
+				printf ("\n El dato fue encontrado y su informacion es:  \n");
+				mostrar_datos(vector, centro, "solicitado" );
+			
+				return centro;
+			
+			}
+			else
+			{
+				if (dato<vector[centro].cod)
+					sup=centro-1;
+				else 
+					inf=centro+1;
+			}
+		
+		}
+		printf("El valor no fue encontrado\n");
 	}
+
 	
-	printf("El valor no fue encontrado");
 	return 0;
+	
 }
 
 
 //------------------------------BUSQUEDA BINARIA  ----------------------------------------------//
 
 
+//------------------------------CANTIDAD DE CD POR TRACKS---------------------------------------//
+void cantidad_tracks(strEstructura vector[], int largoVector){
+	
+	int i, j, contador;
+	
+	for(i = 1; i <= 25; i++){
+		
+		contador = 0;
+		
+		for(j = 0; j <= largoVector; j++ ){
+			
+			if(vector[j].tracks == i){
+				
+				contador++;
+				
+			}
+				
+		}
+		if(contador != 0)
+			printf("Hay %i de CD con %i track.\n", contador, i);
+		
+	}
+	return;
+}
+//------------------------------CANTIDAD DE CD POR TRACKS---------------------------------------//
+
 //------------------------------ MAIN ----------------------------------------------//
 int main()
-{	int largoVector, posMaximo, posMinimo, precioPromedio, i, datoBuscado;
+{	int largoVector, posMaximo, posMinimo, precioPromedio, i;
 	
 	strEstructura vector[500];
 	
 	largoVector=carga(vector);
 	
+	for (i=0; i<largoVector; i++)
+	{
+		printf("Codigo del vector en la posicion %d:: %d \n", i, vector[i].precio);
+	}
 	
 	precioPromedio= precio_promedio(vector, largoVector);
 	
-	printf("\n El precio promedio de los discos es de: %d. \n", precioPromedio);
+	printf("\n El precio promedio de los discos es de: %d.", precioPromedio);
 	system("pause");
+	
+	printf("========================================================================");
 	
 	posMaximo= pos_maximo(vector, largoVector);
 	
-	printf("\n Los datos del valor maximo son:  \n");
+	printf("\n La posicion maxima es: %d. Sus datos son: \n", posMaximo);
 	//muestra los datos del precio maximo
 	mostrar_datos(vector, posMaximo, "de precio maximo");	
  	
 	system("pause");
 	
+	printf("========================================================================");
+	
 	posMinimo= pos_minimo(vector, largoVector);
 	
-	printf("\n Los datos del valor minimo son:  \n");
+	printf("\n La posicion del minimo es: %d \n", posMinimo);
 	
 	//muestra los datos del precio minimo
 	mostrar_datos(vector, posMinimo, "de precio minimo"); 	
 	
-	system("pause");
+
 	
 	ordenar_vector (vector, largoVector);
 	
 	system("pause");
 	
+	printf("========================================================================");
 	
  	mostrar_datos_vector_ordenado(vector, largoVector);
  	
  	system("pause");
 	
+	printf("========================================================================\n");
 	
-	datoBuscado=ingreso_busqueda_binaria(vector, largoVector);
+	ingreso_busqueda_binaria(vector, largoVector);
 	
+	printf("========================================================================\n");
+	
+	cantidad_tracks (vector, largoVector);
 	
 	
 	return 0;
